153d069b95315e943ba897873afa5dc184ff3d03
yhm <15035113008@163.com>
2022-06-07 18:07:11 +0800

    master commit 1 master分支第一次提交

--- src/main/java/com/dj/gittest/controller/GitTestController.java
+++ src/main/java/com/dj/gittest/controller/GitTestController.java
@@ -18,6 +18,7 @@ public class GitTestController {
         System.out.println("hello git~~~");
         System.out.println("hello git~~~~");
         System.out.println("hello git~~~~~");
+        System.out.println("hello master~~~~~");
         return "hello git~~";
     }
 }
