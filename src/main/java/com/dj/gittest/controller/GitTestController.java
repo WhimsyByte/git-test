59f3e206498a503d1e30c8e6cb09ade08e457417
yhm <15035113008@163.com>
2022-06-07 17:45:52 +0800

    first commit 初次提交

---
+++ src/main/java/com/dj/gittest/controller/GitTestController.java
@@ -0,0 +1,20 @@
+package com.dj.gittest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author admin
+ * @version 1.0.0
+ * @ClassName GitTestController.java
+ * @Description TODO
+ * @createTime 2022年06月07日 17:40:00
+ */
+@RestController
+public class GitTestController {
+    @GetMapping("/hello")
+    public String hello() {
+        System.out.println("hello git~~");
+        return "hello git~~";
+    }
+}
